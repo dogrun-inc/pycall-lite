8fa2b9cb9c97bc546008472ac36d57ec2c4c9ebd
constdrop <iota_tz@yahoo.co.jp>
2026-07-19 16:23:26 +0900

    docs: add English RDoc comments to PyCall wrapper

--- lib/pycall.rb
+++ lib/pycall.rb
@@ -1,20 +1,40 @@
 require 'js'
 
+# Lightweight PyCall-compatible bridge for Ruby.wasm + Pyodide.
+#
+# This module provides:
+# - module import via {PyCall.import_module}
+# - wrapper conversion between JS/PyProxy and Ruby values
+# - dynamic method/property/index delegation through {PyCall::PyObject}
 module PyCall
+  # Sets the VM identifier used to resolve the matching Pyodide instance.
+  #
+  # @param vm_id [String] unique VM key managed by JavaScript
   def self.init_vm_id(vm_id)
     @vm_id = vm_id
   end
 
+  # Returns the Pyodide instance associated with this Ruby VM.
+  #
+  # @return [Object] Pyodide instance
   def self.pyodide
     # Fetch Pyodide from the global map using VM ID
     @pyodide ||= JS.global[:__pycall_pyodides__].call(:get, @vm_id)
   end
 
+  # Imports a Python module and wraps the result for Ruby-side access.
+  #
+  # @param mod_name [String, Symbol] Python module name
+  # @return [Object, PyCall::PyObject]
   def self.import_module(mod_name)
     py_mod = pyodide.pyimport(mod_name.to_s)
     wrap(py_mod)
   end
 
+  # Converts JS/Pyodide values into Ruby-friendly values.
+  #
+  # @param py_obj [Object] JavaScript-side value
+  # @return [Object, PyCall::PyObject, nil]
   def self.wrap(py_obj)
     return py_obj unless py_obj.is_a?(JS::Object)
 
@@ -45,7 +65,10 @@ module PyCall
     pyodide.isPyProxy(obj) == true
   end
 
-  # Ruby から JS への基本的な型変換
+  # Performs basic Ruby-to-JS conversion used by delegated calls.
+  #
+  # @param val [Object] Ruby-side value
+  # @return [Object] JavaScript-side value
   def self.ruby_to_js(val)
     case val
     when PyCall::PyObject
@@ -55,13 +78,22 @@ module PyCall
     end
   end
 
+  # Wrapper around a Python object (typically a PyProxy).
+  #
+  # Exposes dynamic property/method/index access from Ruby.
   class PyObject
+    # @return [Object] underlying JavaScript object
     attr_reader :__js_obj__
 
+    # @param js_obj [Object] underlying JavaScript object (PyProxy or similar)
     def initialize(js_obj)
       @__js_obj__ = js_obj
     end
 
+    # Instantiates a Python class object.
+    #
+    # @param args [Array<Object>] constructor arguments
+    # @return [Object, PyCall::PyObject]
     def new(*args)
       # Instantiate a Python class (represented as a Callable JS object)
       js_args = PyCall.ruby_to_js(args)
@@ -69,6 +101,10 @@ module PyCall
       PyCall.wrap(res)
     end
 
+    # Calls a Python callable object.
+    #
+    # @param args [Array<Object>] call arguments
+    # @return [Object, PyCall::PyObject]
     def call(*args)
       # Call a Python function directly
       js_args = PyCall.ruby_to_js(args)
@@ -76,10 +112,20 @@ module PyCall
       PyCall.wrap(res)
     end
 
+    # Handles dynamic property and method access.
+    #
+    # Supports:
+    # - property read: +obj.attr+
+    # - property write: +obj.attr = value+
+    # - method call: +obj.func(arg1, arg2)+
+    #
+    # @param name [Symbol]
+    # @param args [Array<Object>]
+    # @return [Object, PyCall::PyObject]
     def method_missing(name, *args, &block)
       name_str = name.to_s
 
-      # 属性代入: obj.attr = val
+      # Property assignment: obj.attr = val
       if name_str.end_with?('=')
         prop_name = name_str[0...-1]
         val = args[0]
@@ -87,17 +133,17 @@ module PyCall
         return val
       end
 
-      # 属性/メソッドの取得
+      # Property/method resolution
       prop = @__js_obj__[name]
 
       if prop.is_a?(JS::Object) && prop.typeof == 'function'
-        # メソッドの実行
+        # Method execution
         js_args = args.map { |arg| PyCall.ruby_to_js(arg) }
         res = @__js_obj__.call(name, *js_args)
         PyCall.wrap(res)
       else
         if args.any? && prop.is_a?(JS::Object) && prop[:call].typeof == 'function'
-          # 例: python_obj.callable_attr(args)
+          # Example: python_obj.callable_attr(args)
           js_args = args.map { |arg| PyCall.ruby_to_js(arg) }
           res = prop.call(:call, *js_args)
           PyCall.wrap(res)
@@ -111,7 +157,10 @@ module PyCall
       @__js_obj__[name].typeof != 'undefined'
     end
 
-    # インデクスアクセス obj[key]
+    # Index read: +obj[key]+.
+    #
+    # @param key [Object]
+    # @return [Object, PyCall::PyObject]
     def [](key)
       if @__js_obj__[:get].typeof == 'function'
         PyCall.wrap(@__js_obj__.call(:get, PyCall.ruby_to_js(key)))
@@ -120,7 +169,11 @@ module PyCall
       end
     end
 
-    # インデクス設定 obj[key] = val
+    # Index write: +obj[key] = val+.
+    #
+    # @param key [Object]
+    # @param val [Object]
+    # @return [Object] assigned value
     def []=(key, val)
       if @__js_obj__[:set].typeof == 'function'
         @__js_obj__.call(:set, PyCall.ruby_to_js(key), PyCall.ruby_to_js(val))
@@ -130,11 +183,16 @@ module PyCall
       val
     end
 
-    # Pythonオブジェクトの文字列表現
+    # Returns the Python object's string representation.
+    #
+    # @return [String]
     def to_s
       @__js_obj__.call(:toString).to_s
     end
 
+    # Returns a debug representation of the wrapped object.
+    #
+    # @return [String]
     def inspect
       "#<PyCall::PyObject #{@__js_obj__.call(:toString).to_s}>"
     end
